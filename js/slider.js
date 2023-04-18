9762c2e5b66fffe62890c1f44a01345ce2dd45d2
cidector <117948672+cidector@users.noreply.github.com>
2023-04-18 15:32:20 -0500

    Web new
    Cambio total de la web

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,30 @@
 (function(){
-
+    
     const sliders = [...document.querySelectorAll('.testimony__body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
-    let value;
+    let value;   
 
-    buttonNext.addEventListener('click', ()=> {
+    buttonNext.addEventListener('click', ()=>{
         changePosition(1);
-
     });
 
-    buttonBefore.addEventListener('click', ()=> {
+    buttonBefore.addEventListener('click', ()=>{
         changePosition(-1);
     });
 
     const changePosition = (add)=>{
         const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number (currentTestimony);
+        value = Number(currentTestimony);
         value+= add;
 
-        
+
         sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-       if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length : 1;
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
 
-       }
-
-            sliders[value-1].classList.add('testimony__body--show');
+        sliders[value-1].classList.add('testimony__body--show');
 
     }
 
